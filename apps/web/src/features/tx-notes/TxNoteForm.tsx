6d8aa90f39d8aec5ead05e85a6481727c3de298c
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-01-14 16:53:29 +0100

    Fix empty note block

--- apps/web/src/features/tx-notes/TxNoteForm.tsx
+++ apps/web/src/features/tx-notes/TxNoteForm.tsx
@@ -12,5 +12,10 @@ export function TxNoteForm({
   txDetails?: TransactionDetails
   onChange: (note: string) => void
 }) {
+  // @FIXME: update CGW types to include note
+  const note = (txDetails as TransactionDetails & { note: string | null })?.note
+
+  if (!isCreation && !note) return null
+
   return <TxCard>{isCreation ? <TxNoteInput onChange={onChange} /> : <TxNote txDetails={txDetails} />}</TxCard>
 }
