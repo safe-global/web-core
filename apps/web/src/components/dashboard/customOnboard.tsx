83200d0d9bf91cf224bf2e08a0d7303a2180db80
Clóvis Neto <clovisdasilvaneto@gmail.com>
2025-01-15 10:29:44 +0100

    wip custom wallet

--- apps/web/src/components/dashboard/customOnboard.tsx
+++ apps/web/src/components/dashboard/customOnboard.tsx
@@ -2,48 +2,64 @@ import React from 'react'
 import Onboard from '@web3-onboard/core'
 import walletConnectModule from '@web3-onboard/walletconnect'
 
-const wcV2InitOptions = {
+const walletConnect = walletConnectModule({
   projectId: '818ba7e312e2223f49a7dbcf88b1d409',
-  requiredChains: [1, 137], // ETH Mainnet & Polygon
-}
-
-const walletConnect = walletConnectModule(wcV2InitOptions)
-
-const onboard = Onboard({
-  wallets: [walletConnect],
-  chains: [
-    {
-      id: '0x1',
-      token: 'ETH',
-      label: 'Ethereum Mainnet',
-      rpcUrl: 'https://mainnet.infura.io/v3/your-infura-key',
-    },
-    {
-      id: '0x89',
-      token: 'MATIC',
-      label: 'Polygon',
-      rpcUrl: 'https://polygon-rpc.com',
-    },
-  ],
+  requiredChains: [1, 137],
+  version: 2,
+  qrModalOptions: {
+    themeMode: 'dark',
+    explorerExcludedWalletIds: '*',
+    enableExplorer: false,
+  },
 })
 
 function QRCodePOC() {
+  const onboard = Onboard({
+    wallets: [walletConnect],
+    chains: [
+      {
+        id: '0x1',
+        token: 'ETH',
+        label: 'Ethereum Mainnet',
+        rpcUrl: 'https://mainnet.infura.io/v3/your-infura-key',
+      },
+    ],
+    appMetadata: {
+      name: 'Safe',
+      icon: '<your-icon-url>',
+      description: 'Safe Wallet Connection',
+      recommendedInjectedWallets: [],
+      explore: undefined,
+    },
+    connect: {
+      showSidebar: false,
+      disableClose: false,
+    },
+  })
   const openWallet = async () => {
     try {
-      const wallets = await onboard.connectWallet({
+      onboard.state.actions.updateAccountCenter({
+        position: 'topLeft',
+        enabled: true,
+        minimal: true,
+      })
+      await onboard.connectWallet({
         autoSelect: {
           label: 'WalletConnect',
           disableModals: true,
         },
       })
-
-      console.log((wallets[0]?.provider as any)?.connector?.uri)
     } catch (error) {
       console.error('Failed to connect wallet:', error)
     }
   }
 
-  return <button onClick={openWallet}>open wallet</button>
+  return (
+    <>
+      <div id="connect-modal"></div>
+      <button onClick={openWallet}>open wallet</button>
+    </>
+  )
 }
 
 export default QRCodePOC
