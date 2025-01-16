7944a7f4059e4994a463c88b62cd1f2f2932e34a
Germán Martínez <germaga92@gmail.com>
2025-01-16 15:54:11 +0000

    Add safenet and size props to SendAmountBlock component

--- apps/web/src/components/tx-flow/flows/TokenTransfer/SendAmountBlock.tsx
+++ apps/web/src/components/tx-flow/flows/TokenTransfer/SendAmountBlock.tsx
@@ -1,21 +1,25 @@
-import { type ReactNode } from 'react'
-import { type TokenInfo } from '@safe-global/safe-gateway-typescript-sdk'
-import { Box, Typography } from '@mui/material'
 import TokenIcon from '@/components/common/TokenIcon'
 import FieldsGrid from '@/components/tx/FieldsGrid'
 import { formatVisualAmount } from '@/utils/formatters'
+import { Box, Typography } from '@mui/material'
+import { type TokenInfo } from '@safe-global/safe-gateway-typescript-sdk'
+import { type ReactNode } from 'react'
 
 const SendAmountBlock = ({
   amountInWei,
   tokenInfo,
   children,
   title = 'Send:',
+  safenet = false,
+  tokenSize,
 }: {
   /** Amount in WEI */
   amountInWei: number | string
   tokenInfo: Omit<TokenInfo, 'name' | 'logoUri'> & { logoUri?: string }
   children?: ReactNode
-  title?: string
+  title?: string,
+  safenet?: boolean,
+  tokenSize?: number
 }) => {
   return (
     <FieldsGrid title={title}>
@@ -26,7 +30,7 @@ const SendAmountBlock = ({
           gap: 1,
         }}
       >
-        <TokenIcon logoUri={tokenInfo.logoUri} tokenSymbol={tokenInfo.symbol} />
+        <TokenIcon logoUri={tokenInfo.logoUri} tokenSymbol={tokenInfo.symbol} safenet={safenet} size={tokenSize} />
 
         <Typography
           sx={{
